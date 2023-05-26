022001cef4ebf1dfd377cb2ab32b1d7dac8cc57d
Hbence10 <93594678+Hbence10@users.noreply.github.com>
2023-05-26 11:23:27 +0200

    asd

--- 1.py
+++ 1.py
@@ -1,12 +1,12 @@
 class Négyzet ():
     def __init__(self):
         self.a = 0
-    def ter(self):
+    def ter(self): #terület
         return self.a * self.a
-    def ker(self):
+    def ker(self): #kerület
         return self.a*4
     def iratas(self):
-        return "A maga négyzetének a területe: "+ str(Négyzet.ter(self)) + "cm és kerülete: "+ str(Négyzet.ker(self)) +"cm²"
+        return "A maga négyzetének a területe: "+ str(Négyzet.ter(self)) + "cm² és kerülete: "+ str(Négyzet.ker(self)) +"cm"
 
 négyzet = Négyzet()
 print("Az adatok centiméterben értendőek")
